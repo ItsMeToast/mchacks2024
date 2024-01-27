46ddbf67017969a3365eff36ca5eb949fcce7407
ItsMeToast <ItsMeToastyBread@gmail.com>
2024-01-27 18:16:42 -0500

    Flask Structure
    setup flask project structure for the web app

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.get("/")
-def hello():
-    """Return a friendly HTTP greeting."""
-    return "Hello World, this is a test!\n"
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
 
 
 if __name__ == "__main__":
