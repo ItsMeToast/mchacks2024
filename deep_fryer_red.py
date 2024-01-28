f9a60d6c8e7de27d62ec03d1a34844217382626b
Skylar-gu <157906407+Skylar-gu@users.noreply.github.com>
2024-01-28 01:56:22 -0500

    Update deep_fryer_red.py

--- deep_fryer_red.py
+++ deep_fryer_red.py
@@ -3,12 +3,11 @@ import numpy as np
 import requests
 from io import BytesIO
 
-def deep_fry_red(image_url, orange_factor=6.5, yellow_factor=1.2, brightness_factor=1.4, contrast_factor=1.2):
-    bf_reset, of_reset, yf_reset = [1, 1, 1]
-    
-    if fry_factor == 1:
+def deep_fry_red(image_url, fry_level=3, orange_factor=6.5, yellow_factor=1.2, brightness_factor=1.4, contrast_factor=1.2):
+    # deep fries image based on fry_level
+    if fry_level == 1:
         brightness_factor, orange_factor, yellow_factor = [1, 1, 1]
-    if fry_factor == 2:
+    if fry_level == 2:
         brightness_factor *= 0.35
         orange_factor *= 0.35
         
@@ -30,7 +29,7 @@ def deep_fry_red(image_url, orange_factor=6.5, yellow_factor=1.2, brightness_fac
     enhancer_b = ImageEnhance.Brightness(enhanced_image)
     enhancer_c = ImageEnhance.Contrast(enhanced_image)
     
-    if fry_factor != 1:
+    if fry_level != 1:
         enhanced_image = enhancer_b.enhance(brightness_factor)
         enhanced_image = enhancer_c.enhance(contrast_factor)
 
