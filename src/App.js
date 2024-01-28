9476448bb8765fa2949a74ca2bd2db708de59e25
ItsMeToast <ItsMeToastyBread@gmail.com>
2024-01-28 01:53:30 -0500

    Update App.js Template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="app-container container text-center d-flex flex-column h-100 p-0 pt-5">
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+
+      <div class="title row justify-content-center">Memeify</div>
+      <div class="row justify-content-center">
+        <button class="button">Generate meme!</button>
+      </div>
     </div>
   );
 }
